620052a19042a6da0ec64822b5d51b91d71b8e59
Matt Weber <matt@weberdesign-ak.com>
2018-12-07 18:19:38 -0900

    Added as git repos... but half wont work with ubuntu

--- paths-service-master/client/src/index.js
+++
@@ -1,4 +0,0 @@
-import * as NTPathsService from './components/App.js';
-
-ReactDOM.render(<NTPathsService.CanonicalPath/>, document.getElementById('9Trails.PathsService.CanonicalPath'));
-ReactDOM.render(<NTPathsService.Recordings/>, document.getElementById('9Trails.PathsService.Recordings'));
