1723dacc455a7dc5800afb92b45cff7e644783c4
Matt Weber <matt@weberdesign-ak.com>
2018-12-15 12:39:20 -0900

    ID detection

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/:trailId(\\d+$)*?', function(req, res) {
+    res.status(200).sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(port, () => {
     console.log(`server running at: http://localhost:${port}`);
 });
